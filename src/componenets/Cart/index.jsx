f2bac89686d0fbc98c09a58aab26da5877f5bb92
mallah-elmehdi <elmehdimallah@gmail.com>
2025-04-05 16:48:08 +0000

    add the cart mechanism

--- src/componenets/Cart/index.jsx
+++ src/componenets/Cart/index.jsx
@@ -1,10 +1,12 @@
-import { Drawer, Fade, Grow, IconButton, Stack, Typography, Box, Badge } from '@mui/material';
-import { blue } from '@mui/material/colors';
-import React, { useEffect } from 'react'
-import { CiShoppingCart } from 'react-icons/ci';
+import { Drawer, Fade, Grow, IconButton, Stack, Typography, Box, Badge, Divider, Button, Chip } from '@mui/material';
+import { blue, grey, yellow } from '@mui/material/colors';
+import React, { useEffect, useState } from 'react'
+import { CiShoppingCart, CiWarning } from 'react-icons/ci';
 import { useDispatch, useSelector } from 'react-redux';
 import Img from '../Img';
 import { closeDrawer, openDrawer } from '../../features/drawer';
+import { BsDash, BsPlus } from 'react-icons/bs';
+import { FormControl, FormHelperText, FormLabel, Input } from '@mui/joy';
 
 const Cart = () => {
     const dispatch = useDispatch()
@@ -13,6 +15,8 @@ const Cart = () => {
     const { isDrawerOpen } = useSelector(state => state.drawer)
 
     const [skip, setSkip] = React.useState({});
+    const [weekCount, setWeekCount] = useState(1);
+    const [error, setError] = useState(false);
 
     const { backgroudImage, size, hire_period_days, price_before_vat, vat, allowed_on_road } = skip
 
@@ -26,8 +30,29 @@ const Cart = () => {
     useEffect(() => {
         if (selectedSkips.length > 0) {
             setSkip(selectedSkips[0])
+            setWeekCount(1)
+            setError(false)
         }
     }, [selectedSkips])
+
+    // ------------------ the period control
+
+
+
+    const handleWeekCountUp = () => {
+        if (weekCount < (hire_period_days / 7)) {
+            setWeekCount(weekCount + 1)
+        } else {
+            setError(true)
+        }
+    }
+
+    const handleWeekCountDown = () => {
+        if (weekCount > 1) {
+            setWeekCount(weekCount - 1)
+        }
+        setError(false)
+    }
 
     return (
         <>
@@ -37,7 +62,7 @@ const Cart = () => {
                 right: 25,
                 zIndex: 11,
             }}>
-                <Grow in={selectedSkips.length > 0 && !open} timeout={300}>
+                <Grow in={selectedSkips.length > 0} timeout={300}>
                     <div>
                         <IconButton onClick={handleOpen}
                             sx={{
@@ -59,15 +84,152 @@ const Cart = () => {
                 </Grow>
             </Box>
             <Drawer anchor='right' open={isDrawerOpen} onClose={handleClose}>
-                <Stack spacing={3} sx={{ p: 3, width: 350 }}>
-                    <Img src={backgroudImage} />
+                <Stack spacing={2} sx={{ p: 3, width: 350, height: "100%" }}>
+                    <Box position="relative">
+                        <Img src={backgroudImage} />
+                        {allowed_on_road === false &&
+                            <Chip
+                                icon={<CiWarning />}
+                                sx={{
+                                    backgroundColor: yellow[600],
+                                    color: grey[900],
+                                    fontSize: 10,
+                                    fontWeight: 'light',
+                                    position: "absolute",
+                                    bottom: "10px",
+                                    left: "7px",
+                                    py: 0,
+                                    '.MuiChip-icon': {
+                                        color: grey[900],
+                                        fontSize: 12,
+                                    },
+                                    px: 1,
+                                }}
+                                size='small'
+                                label="Private Property Only"
+                            />
+                        }
+                    </Box>
+
+                    <Chip
+                        sx={{
+                            background: `linear-gradient(193deg, ${blue[500]} 20%, ${blue[700]} 70%)`,
+                            borderRadius: 2,
+                        }}
+                        label={
+                            <Typography fontSize={20} lineHeight={1} fontWeight="bold" color="white">
+                                {size}{' '}
+                                <Typography component='span' fontSize={12} fontWeight="light" color="white">
+                                    Yards Skip
+                                </Typography>
+                            </Typography>
+                        }
+                    />
+                    <Divider sx={{ borderColor: grey[100] }} />
                     <Stack spacing={1} >
-                        <Typography fontSize={20} lineHeight={1} fontWeight="bold" color='grey.900'>
-                            Hire period
+                        <FormControl error={error}>
+                            <FormLabel> Please choose the hiring period</FormLabel>
+                            <Stack direction="row" alignItems="center" spacing={1}>
+                                <IconButton
+                                    disableRipple
+                                    sx={{
+                                        backgroundColor: 'primary.main',
+                                        color: 'white'
+                                    }}
+                                    onClick={handleWeekCountUp}
+                                >
+                                    <BsPlus />
+                                </IconButton>
+                                <Input
+                                    type='number'
+                                    value={weekCount}
+                                    sx={{
+                                        flexGrow: 1
+                                    }}
+                                    endDecorator={
+                                        <Typography component='span' fontSize={12} fontWeight="light" color={error ? "error" : grey[900]}>
+                                            per week
+                                        </Typography>
+                                    }
+                                />
+                                <IconButton
+                                    disableRipple
+                                    sx={{
+                                        backgroundColor: 'primary.main',
+                                        color: 'white'
+                                    }}
+                                    onClick={handleWeekCountDown}
+                                >
+                                    <BsDash />
+                                </IconButton>
+                            </Stack>
+                            {
+                                error && <FormHelperText>
+                                    This skip is only availabe for {hire_period_days / 7} weeks
+                                </FormHelperText>
+                            }
+                        </FormControl >
+                    </Stack >
+
+                    <Divider sx={{ borderColor: grey[100] }} />
+                    <Stack spacing={1} >
+                        <Typography fontSize={15} lineHeight={1} fontWeight="lighter" color='grey.900'>
+                            Price before VAT
                         </Typography>
-                        <Typography fontSize={20} lineHeight={1} fontWeight="bold" color='white'>
-                            {hire_period_days} days
+                        <Typography fontSize={20} lineHeight={1} fontWeight={300} color='grey.900'>
+                            £{price_before_vat * weekCount || "0.00"}{' '}
                         </Typography>
+                    </Stack>
+                    <Stack spacing={1} >
+                        <Typography fontSize={15} lineHeight={1} fontWeight="lighter" color='grey.900'>
+                            VAT ({vat || 0}%)
+                        </Typography>
+                        <Typography fontSize={20} lineHeight={1} fontWeight={300} color='grey.900'>
+                            £{(price_before_vat * vat / 100).toFixed(2) * weekCount}
+                        </Typography>
+                    </Stack>
+                    <Stack spacing={1} flexGrow={1}>
+                        <Typography fontSize={15} lineHeight={1} fontWeight="lighter" color='grey.900'>
+                            Total price
+                        </Typography>
+                        <Typography fontSize={25} lineHeight={1} fontWeight="bold" color='grey.900'>
+                            £{(price_before_vat * (1 + vat / 100)).toFixed(2) * weekCount}
+                        </Typography>
+                    </Stack>
+                    <Stack direction="row" alignItems="center" spacing={1}>
+                        <Button
+                            size='medium'
+                            sx={{
+                                fontSize: 15,
+                                fontWeight: 300,
+                                textTransform: "capitalize",
+                                borderRadius: 2,
+                                flexGrow: 1
+                            }}
+                            disableRipple
+                            disableElevation
+                            variant='contained'
+                            color='inherit'
+                            onClick={handleClose}
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            size='medium'
+                            sx={{
+                                fontSize: 15,
+                                fontWeight: 300,
+                                textTransform: "capitalize",
+                                borderRadius: 2,
+                                flexGrow: 1
+                            }}
+                            disableRipple
+                            disableElevation
+                            variant='contained'
+                            color='success'
+                        >
+                            Continue
+                        </Button>
                     </Stack>
                 </Stack>
             </Drawer>
